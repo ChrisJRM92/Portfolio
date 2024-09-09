81bda5f1d5157a80c7f8f7ee3e9f3275197bbc2d
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-08 21:29:23 -0500

    New portfolio react proyect
    New portfolio react proyect

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/App.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container'>
+        <div class="lateralInfo"> </div>
+        <div class="topMenu"> </div>
+        <div class="content"> </div>
+        <div class="bottomMenu"> </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './styles/index.scss'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
