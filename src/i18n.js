307c9257c51305ac287d23f28536f00919278c00
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 12:41:46 -0500

    Test 5
    Test 5

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,17 @@ import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 
-
 i18n.use(Backend).use(initReactI18next).init({
   backend: {
-    loadPath: './public/locales/{{lng}}.json', // Ruta desde la carpeta public
+    loadPath: '../public/locales/{{lng}}.json', // Ruta desde la carpeta public
   },
+  
   lng: 'es', // Idioma por defecto
   fallbackLng: 'es',
   interpolation: {
     escapeValue: false,
   },
+
 });
 
 export default i18n;
