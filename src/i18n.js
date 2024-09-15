e9f783e9a9fc6326c8b241fdc7c4ede062561671
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 11:37:52 -0500

    Test issue i18next
    Test issue i18next

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 
-import enTranslations from '/Locales/en.json';
-import esTranslations from '/locales/es.json';
+// import enTranslations from '/Locales/en.json';
+// import esTranslations from '/locales/es.json';
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -13,6 +13,9 @@ i18n.use(initReactI18next).init({
   fallbackLng: 'es',
   interpolation: {
     escapeValue: false,
+  },
+  backend: {
+    loadPath: '/locales/{{lng}}.json', // Cambia esta línea según la estructura de tu proyecto
   },
 });
 
