48034a36bf33c5938ba23acefddf4c53c2c0bcb4
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 11:26:47 -0500

    Test deploy
    Test deploy

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 
-import enTranslations from '../public/Locales/en.json';
-import esTranslations from '../public/locales/es.json';
+import enTranslations from '/Locales/en.json';
+import esTranslations from '/locales/es.json';
 
 i18n.use(initReactI18next).init({
   resources: {
