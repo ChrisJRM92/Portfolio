ba6d71e4ce5506b8f5159c7ad23f7e701c610b63
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 11:23:48 -0500

    Static files
    Static files

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 
-import enTranslations from './locales/en.json';
-import esTranslations from './locales/es.json';
+import enTranslations from '../public/Locales/en.json';
+import esTranslations from '../public/locales/es.json';
 
 i18n.use(initReactI18next).init({
   resources: {
