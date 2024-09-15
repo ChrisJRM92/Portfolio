992615d36dda8f9ad2e41b7aff6e108a7e20af72
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 12:09:46 -0500

    Test bug
    Test bug

--- src/i18n.js
+++ src/i18n.js
@@ -2,17 +2,11 @@ import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 
-// import enTranslations from '../public/locales/en.json';
-// import esTranslations from '../public/locales/es.json';
 
 i18n.use(Backend).use(initReactI18next).init({
   backend: {
     loadPath: '/locales/{{lng}}.json', // Ruta desde la carpeta public
   },
-  // resources: {
-  //   en: { translation: enTranslations },
-  //   es: { translation: esTranslations },
-  // },
   lng: 'es', // Idioma por defecto
   fallbackLng: 'es',
   interpolation: {
