2d234949c52b8a18d2ba17a246cb015f6926baf6
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-15 19:50:30 -0500

    Added EmailJS ENV
    Added EmailJS ENV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
 import { LuSend } from "react-icons/lu";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
+
 import './styles/Content.scss'
 import { useTranslation } from 'react-i18next';
 
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
   const { t } = useTranslation();
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(import.meta.env.VITE_SERVICE_ID_EMAILJS, import.meta.env.VITE_TEMPLATE_ID_EMAILJS, form.current, {
+        publicKey: import.meta.env.VITE_PUBLIC_KEY_EMAILJS,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className="container_contact">
-      <form className="form" action="">
+      <form className="form" ref={form} onSubmit={sendEmail}>
         <div className="form_card1">
           <p id="contact_section_description" className="description">{t('contact_section_description')}</p>
           <div className="contact_div">
@@ -35,18 +57,18 @@ const Contact = () => {
         </div>
         <div className="form_card2">
           <div className="form_elements">
-            <input className="form_inputs" type="text" placeholder={t('placeholders.name')} />
+            <input className="form_inputs" type="text" name="name" placeholder={t('placeholders.name')} />
           </div>
           <div className="form_elements">
-            <input className="form_inputs" type="email" placeholder={t('placeholders.email')} />
+            <input className="form_inputs" type="email" name="email" placeholder={t('placeholders.email')} />
           </div>
           <div className="form_elements">
-            <input className="form_inputs" type="text" placeholder={t('placeholders.subject')} />
+            <input className="form_inputs" type="text" name="subject" placeholder={t('placeholders.subject')} />
           </div>
           <div className="form_elements">
-            <textarea className="form_inputs text_area" name="" id="" placeholder={t('placeholders.text_area')}></textarea>
+            <textarea className="form_inputs text_area" name="message" placeholder={t('placeholders.text_area')}></textarea>
           </div>
-          <button id="btn_send_message" className="btn_submit" type="submit">{t('btn_send_message')} &nbsp;<LuSend /> </button>
+          <button id="btn_send_message" className="btn_submit" type="submit" value="Send">{t('btn_send_message')} &nbsp;<LuSend /> </button>
         </div>
       </form>
     </div>
