7e8587ffece2db1de87b445e388a0b95bc853ac8
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-16 22:15:56 -0500

    Added component info
    Added component info

--- src/components/BottomInfo.jsx
+++ src/components/BottomInfo.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { IoQrCode } from "react-icons/io5";
+import { HiOutlineDownload } from "react-icons/hi";
+import './styles/BottomInfo.scss'
 
 const BottomInfo = () => {
   return (
-    <div>
-      <button>Portafolio</button>
-      <button>Proyectos</button>
+    <div className="container_details_data">
+      <a className='my_proyects' href="#portfolio">Mis proyectos &nbsp;<p className="qr_icon"><IoQrCode /></p></a>
+      <a className='dowload_cv'>Descargar CV &nbsp;<p className="qr_icon"><HiOutlineDownload /></p></a>
     </div>
   )
 }
