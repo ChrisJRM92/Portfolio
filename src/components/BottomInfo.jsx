36d4bf8eff8ea76bf0a7672143f90615e94f58ef
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-17 22:24:29 -0500

    Modified data
    Modified data

--- src/components/BottomInfo.jsx
+++ src/components/BottomInfo.jsx
@@ -9,7 +9,7 @@ const BottomInfo = () => {
   return (
     <div className="container_details_data">
       <a className='my_proyects' href="#portfolio" id="my_proyects">{t('my_proyects')} &nbsp;<p className="qr_icon"><IoQrCode /></p></a>
-      <a className='dowload_cv' href="https://drive.google.com/drive/folders/1GN4DEvATzeZL3PQAhoC8A8hbyAJCS-na?usp=sharing" id="download_cv">{t('download_cv')} &nbsp;<p className="qr_icon"><HiOutlineDownload /></p></a>
+      <a className='dowload_cv' href="https://drive.google.com/drive/folders/1GN4DEvATzeZL3PQAhoC8A8hbyAJCS-na?usp=sharing" target="_blank" id="download_cv">{t('download_cv')} &nbsp;<p className="qr_icon"><HiOutlineDownload /></p></a>
     </div>
   )
 }
