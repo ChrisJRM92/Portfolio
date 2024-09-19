25b2bdadfd59c7aafeacebbf92d8d4521f5175a9
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-09-19 16:51:39 -0500

    Fixed bug and added new data
    Fixed bug and added new data

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -16,10 +16,11 @@ import { FaGithub } from "react-icons/fa";
 import { SiRailway } from "react-icons/si";
 import { SiRender } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
-import { IoIosArrowUp } from "react-icons/io";
+// import { IoIosArrowUp } from "react-icons/io";
 import ArrowDown from "./ArrowDown";
 import { useTranslation } from 'react-i18next';
 import Contact from "./Contact";
+import { Carousel } from 'nuka-carousel';
 
 const Content = () => {
   const { t } = useTranslation();
@@ -111,6 +112,28 @@ const Content = () => {
         <ArrowDown />
       </div>
 
+      <div id="about" className="about">
+        <button id="btn_about_section" className="btn_section"><p className="icon_star"><PiStarFourFill /></p>&nbsp;{t('btn_about_section')}</button>
+        <h2 id="about_title" className="title_section">{t('about_title')}</h2>
+        <p id="about_description" className="description">{t('about_description')}</p>
+        <br />
+        <p id="about_description2" className="description">{t('about_description2')}</p>
+        <div className="container_img">
+          {/* <div style={{ backgroundImage: "url('/me_1.jpg')" }} className="container_photo"></div> */}
+          {/* <div style={{ backgroundImage: "url('/me_2.jpg')" }} className="container_photo"></div> */}
+          {/* <div style={{ backgroundImage: "url('/me_3.jpg')" }} className="container_photo"></div> */}
+        </div>
+        <h2 id="skills_about" className="title_section_skills">{t('skills_about')}</h2>
+        <div className="container_skills">
+          <button id="skill_1" className="skills">{t('skill_1')}</button>
+          <button id="skill_2" className="skills">{t('skill_2')}</button>
+          <button id="skill_3" className="skills">{t('skill_3')}</button>
+          <button id="skill_4" className="skills">{t('skill_4')}</button>
+          <button id="skill_5" className="skills">{t('skill_5')}</button>
+          <button id="skill_6" className="skills">{t('skill_6')}</button>
+        </div>
+      </div>
+
       <div id="portfolio" className="portfolio">
         <button id="btn_portfolio_content" className="btn_section"><p className="icon_star"><PiStarFourFill /></p>&nbsp;{t('btn_portfolio_content')}</button>
         <h2 id="sub_title_1" className="title_section">{t('sub_title_1')}</h2>
@@ -129,47 +152,27 @@ const Content = () => {
         </div>
       </div>
 
-      <div id="about" className="about">
-        <button id="btn_about_section" className="btn_section"><p className="icon_star"><PiStarFourFill /></p>&nbsp;{t('btn_about_section')}</button>
-        <h2 id="about_title" className="title_section">{t('about_title')}</h2>
-        <p id="about_description" className="description">{t('about_description')}</p>
-        <div className="container_img">
-          <div style={{ backgroundImage: "url('/me_1.jpg')" }} className="container_photo"></div>
-          <div style={{ backgroundImage: "url('/me_2.jpg')" }} className="container_photo"></div>
-          <div style={{ backgroundImage: "url('/me_3.jpg')" }} className="container_photo"></div>
-        </div>
-        <h2 id="skills_about" className="title_section_skills">{t('skills_about')}</h2>
-        <div className="container_skills">
-          <button id="skill_1" className="skills">{t('skill_1')}</button>
-          <button id="skill_2" className="skills">{t('skill_2')}</button>
-          <button id="skill_3" className="skills">{t('skill_3')}</button>
-          <button id="skill_4" className="skills">{t('skill_4')}</button>
-          <button id="skill_5" className="skills">{t('skill_5')}</button>
-          <button id="skill_6" className="skills">{t('skill_6')}</button>
-        </div>
-      </div>
-
       <div id="experience" className="experience">
         <button id="btn_knowledge" className="btn_section"><p className="icon_star"><PiStarFourFill /></p>{t('btn_knowledge')}</button>
         <h2 id="experience_title" className="title_section">{t('experience_title')}</h2>
         <p id="experience_description" className="description">{t('experience_description')}</p>
         <div className="container_exp">
-          <div className="skill_icon"><FaHtml5 /></div>
-          <div className="skill_icon"><FaCss3Alt /></div>
-          <div className="skill_icon"><FaJsSquare /></div>
-          <div className="skill_icon"><FaReact /></div>
-          <div className="skill_icon"><FaSass /></div>
-          <div className="skill_icon"><FaNpm /></div>
-          <div className="skill_icon"><FaNode /></div>
-          <div className="skill_icon"><BiLogoPostgresql /></div>
-          <div className="skill_icon"><IoLogoVercel /></div>
-          <div className="skill_icon"><FaGithub /></div>
-          <div className="skill_icon"><SiRailway /></div>
-          <div className="skill_icon"><SiRender /></div>
-          <div className="skill_icon"><FaAws /></div>
+          <div className="skill_icon"><FaHtml5 /><p>HTML5</p></div>
+          <div className="skill_icon"><FaCss3Alt /><p>CSS3</p></div>
+          <div className="skill_icon"><FaJsSquare /><p>Javascript</p></div>
+          <div className="skill_icon"><FaReact /><p>React</p></div>
+          <div className="skill_icon"><FaSass /><p>Sass</p></div>
+          <div className="skill_icon"><FaNpm /><p>npm</p></div>
+          <div className="skill_icon"><FaNode /><p>NodeJs</p></div>
+          <div className="skill_icon"><BiLogoPostgresql /><p>PostgreSQL</p></div>
+          <div className="skill_icon"><IoLogoVercel /><p>Vercel</p></div>
+          <div className="skill_icon"><FaGithub /><p>Github</p></div>
+          <div className="skill_icon"><SiRailway /><p>Railway</p></div>
+          <div className="skill_icon"><SiRender /><p>Render</p></div>
+          <div className="skill_icon"><FaAws /><p>Amazon web services</p></div>
         </div>
       </div>
-      
+
       <div id="contact_main_content" className="contact">
         <button id="btn_contact_section" className="btn_section"><p className="icon_star"><PiStarFourFill /></p>&nbsp;{t('btn_contact_section')}</button>
         <h2 id="contact_title_section" className="title_section">{t('contact_title_section')}</h2>
--- src/components/LateralContent.jsx
+++ src/components/LateralContent.jsx
@@ -26,8 +26,8 @@ const LateralContent = () => {
         <a href="https://github.com/ChrisJRM92" target="_blank" className='social'><FaGithub /></a>
         <a href="https://www.linkedin.com/in/chrisjrm/" target="_blank" className='social'><FaLinkedin /></a>
         <a href="https://wa.me/+593999796892?text=Hola Christian, vi tu portafolio, ¿podríamos planificar una reunión?" target="_blank" className='social'><FaWhatsapp /></a>
-        <a href="https://www.facebook.com/CJRM92/" target="_blank" className='social'><FaFacebook /></a>
-        <a href="https://www.instagram.com/cris_s6662024/" target="_blank" className='social'><FaInstagram /></a>
+        {/* <a href="https://www.facebook.com/CJRM92/" target="_blank" className='social'><FaFacebook /></a> */}
+        {/* <a href="https://www.instagram.com/cris_s6662024/" target="_blank" className='social'><FaInstagram /></a> */}
       </div>
       <div className="btn">
         <a href="#contact_main_content" id="contact" className="btn_contact">{t('contact')}</a>
